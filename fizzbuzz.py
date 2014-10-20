c93c4944e9a9b31818440cf3d0217c9a2a767651
Michele <michele.morelock@gmail.com>
2014-10-20 23:55:53 +0000

    Creating version 2 that takes user inputs for fizzbuzz.

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,17 +1,32 @@
 # Creating the FizzBuzz app from scratch.
 
-# For FizzBuzz v1, we are going to print all the numbers
-# from 1 to 100. For every number divisible by 3, we will print 
+# FizzBuzz v2, time to prompt for inputs!  Either run this 
+# script with a minimum and maximum number - not inclusive. If
+# the user does not supply arguments, they will be prompted
+# for two numbers.
+#
+# FizzBuzz game:
+# For every number divisible by 3, we will print 
 # fizz instead of the number.  For every number divisible by 5,
 # we will print buzz.  If the number is divisible by both, we
 # will print fizzbuzz.
 
-for i in range(1,101):
-    if i % 3 == 0 and i % 5 == 0:
+import sys
+
+if len(sys.argv) == 1:
+    min_num = int(raw_input("Please enter min number: "))
+    max_num = int(raw_input("Please enter a max number: "))
+else:
+    min_num = int(sys.argv[1])
+    max_num = int(sys.argv[2])
+
+
+for num in range(min_num, max_num):
+    if num % 3 == 0 and num % 5 == 0:
         print 'fizzbuzz'
-    elif i % 3 == 0:
+    elif num % 3 == 0:
         print 'fizz'
-    elif i % 5 == 0:
+    elif num % 5 == 0:
         print 'buzz'
     else:
-        print i
+        print num
